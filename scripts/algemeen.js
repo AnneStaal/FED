6a7e99637370ee4bb9f3f32e20d17e55d41f326a
AnneStaal <anne.staal@hva.nl>
2025-11-27 16:17:46 +0100

    Add files via upload

--- scripts/algemeen.js
+++ scripts/algemeen.js
@@ -5,8 +5,8 @@
 /* JOUW CODE HIER - stap 4 */
 
 // stap 1: zoek de menu-button op en sla die op in een variabele
-const menuButton = document.querySelector("header section nav button");
-const deNav = document.querySelector("header section");
+const menuButton = document.querySelector("header nav button");
+const deNav = document.querySelector("header");
 
 // stap 2: laat de menu-button luisteren naar kliks en voer dan een functie uit
 menuButton.onclick = toggleMenu;
@@ -14,5 +14,5 @@ menuButton.onclick = toggleMenu;
 // stap 3: voeg in de functie een class toe aan de nav
 function toggleMenu(){
   deNav.classList.toggle("is-open");
-  
+
 }
